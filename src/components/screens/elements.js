1826609250ce97bc5ab0f733d61a171683a88911
Chisom Timothy Oguine <89150318+chilara@users.noreply.github.com>
2023-09-26 11:53:11 +0100

    element title page in progress

--- src/components/screens/elements.js
+++ src/components/screens/elements.js
@@ -1,7 +1,18 @@
 import React from "react";
+import right_arrow from "../../assets/right_arrow.png";
 
 const Elements = () => {
-  return <div className="elements"></div>;
+  return (
+    <div className="elements">
+      <div className="page_title">
+        <p className="text_1">Payroll Management</p>
+        <img src={right_arrow} alt="arrow" img_group />
+        <p className="text_1">Element Setup</p>
+        <img src={right_arrow} alt="arrow" img_group />
+        <p className="text_2">Elements</p>
+      </div>
+    </div>
+  );
 };
 
 export default Elements;
